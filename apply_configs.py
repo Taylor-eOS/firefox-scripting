cdab474fc7ed27964c80c801d05187b4976da9f7
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-02-26 05:24:29 +0100

    Typo

--- apply_configs.py
+++ apply_configs.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
         #"widget.non-native-theme.enabled": (true, False),
         "toolkit.zoomManager.zoomValues": (".98,.99,1,1.01,1.02,1.03,1.04,1.05,1.06,1.07,1.08,1.09,1.1", True),
         "browser.urlbar.trimURLs": ("false", False),
-        "dialog_enable_delay": ("10", False),
         "browser.urlbar.maxRichResults": ("6", False),
         "browser.bookmarks.max_backups": ("4", False)}
     update_firefox_prefs(profile_path, prefs_to_update)
